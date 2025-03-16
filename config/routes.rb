b974deddb1cf98f11c088367b1c711e267443870
mkaho9642 <193476776+mkaho9642@users.noreply.github.com>
2025-03-16 18:42:46 +0000

    [Update] routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'post_images/new'
-  get 'post_images/index'
-  get 'post_images/show'
   devise_for :users
   root to: "homes#top"
+
+  resources :post_images, only: [:new, :index, :show]
+
   get 'homes/about', to: 'homes#about', as: 'about'
 end
