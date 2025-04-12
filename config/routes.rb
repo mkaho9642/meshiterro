ae9604688166c18dbb532218a920e70b8206e109
mkaho9642 <193476776+mkaho9642@users.noreply.github.com>
2025-04-12 10:42:34 +0000

    [Add] Admin

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
     sessions: 'admin/sessions'
   }
 
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy]
+  end
+
   devise_for :users
   root to: "homes#top"
   get 'homes/about', to: 'homes#about', as: :about
@@ -10,6 +15,17 @@ Rails.application.routes.draw do
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resource :favorite, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
+  end
+
+  scope module: :public do
+    devise_for :users
+    root to: 'homes#top'
+    get 'homes/about', to: 'homes#about', as: :about
+    resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+      resource :favorites, only: [:create, :destroy]
+      resources :post_comments, only: [:create, :destroy]
+    end
+    resources :users, only: [:show, :edit, :update]
   end
 
   resources :users, only: [:show, :edit, :update]
